0983fa7b9215197c80d26cdc0b72d91a81cd737b
Leo Kwok <112677134+KwokHangMing@users.noreply.github.com>
2023-09-30 15:17:36 +0000

    dialogflow test

--- Google_cloud/main.tf
+++ Google_cloud/main.tf
@@ -19,3 +19,31 @@ resource "google_storage_bucket" "bucket" {
   name     = "220011928-bucket"
   location = "US"
 }
+
+resource "google_dialogflow_cx_agent" "full_agent" {
+  display_name = "dialogflowcx-agent"
+  location = "global"
+  default_language_code = "en"
+  time_zone = "Hong Kong"
+  description = "This is a test agent."
+  enable_stackdriver_logging = true
+  enable_spell_correction    = true
+    speech_to_text_settings {
+        enable_speech_adaptation = true
+    }
+}
+
+resource "google_dialogflow_cx_entity_type" "basic_entity_type" {
+  parent       = google_dialogflow_cx_agent.full_agent.id
+  display_name = "MyEntity"
+  kind         = "KIND_MAP"
+  entities {
+    value = "value1"
+    synonyms = ["synonym1","synonym2"]
+  }
+  entities {
+    value = "value2"
+    synonyms = ["synonym3","synonym4"]
+  }
+  enable_fuzzy_extraction = false
+} 
