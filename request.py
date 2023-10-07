cc5c5d91e426169e3c3fa49f8a842fc01b6e445e
KwokHangMing <220011928@stu.vtc.edu.hk>
2023-10-07 22:36:17 +0800

    functions-framework plugins

--- request.py
+++ request.py
@@ -1,19 +1,23 @@
-import requests
-import json
+import functions_framework
 
-url = "https://api.dialogflow.com/v1/query?v=20150910"
-headers = {
-    'content-type': 'application/json',
-    'Authorization': 'Bearer YOUR_CLIENT_ACCESS_TOKEN',
-    'Accept': 'application/json'
-}
-payload = {
-    "contexts": ["shop"],
-    "lang": "en",
-    "query": "YOUR_QUERY",
-    "sessionId": "12345",
-    "timezone": "America/New_York"
-}
+@functions_framework.http
+def make_request(request):
+    # """
+    # HTTP Cloud Function that makes another HTTP request.
+    # Args:
+    #     request (flask.Request): The request object.
+    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    # Returns:
+    #     The response text, or any set of values that can be turned into a
+    #     Response object using `make_response`
+    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    # """
+    import requests
 
-response = requests.post(url, data=json.dumps(payload), headers=headers)
-print(response.json)
+    # The URL to send the request to
+    url = "http://example.com"
+
+    # Process the request
+    response = requests.get(url)
+    response.raise_for_status()
+    return "Success!"
